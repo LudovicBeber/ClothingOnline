a1b2a26db7a4d3eaa1871910806a2fad6eb83534
Ludovic Bernard <ludovic.bernard.etu@univ-lille.fr>
2026-01-18 16:37:57 +0100

    Create project

---
+++ screens/clothes/ClothesListScreen.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const ClothesListScreen = () => {
+
+    return (
+        <View>
+            <Text>Liste de vêtements disponibles :</Text>
+        </View>
+    );
+
+}
+
+export default ClothesListScreen;
